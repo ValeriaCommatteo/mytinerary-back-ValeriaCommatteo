6d78532ea854cc4d327775135f5f0846347a4ac9
ValeriaCommatteo <valc2012@hotmail.com>
2023-08-23 13:45:04 -0300

    add config

---
+++ src/cities.js
@@ -0,0 +1,100 @@
+const cities = [
+    {
+        "id": 1,
+        "imagen": "Agua.jpg",
+        "nombre": "Imagen 1",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 1"
+    },
+    {
+        "id": 2,
+        "imagen": "Arg.jpg",
+        "nombre": "Imagen 2",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 2"
+    },
+    {
+        "id": 3,
+        "imagen": "BsAs.jpg",
+        "nombre": "Imagen 3",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 3"
+    },
+    {
+        "id": 4,
+        "imagen": "Cataratas.jpg",
+        "nombre": "Imagen 4",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 4"
+    },
+    {
+        "id": 5,
+        "imagen": "España.jpg",
+        "nombre": "Imagen 5",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 5"
+    },
+    {
+        "id": 6,
+        "imagen": "Ruinas.jpg",
+        "nombre": "Imagen 6",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 6"
+    },
+    {
+        "id": 7,
+        "imagen": "Ny.jpg",
+        "nombre": "Imagen 7",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 7"
+    },
+    {
+        "id": 8,
+        "imagen": "Grecia.jpg",
+        "nombre": "Imagen 8",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 8"
+    },
+    {
+        "id": 9,
+        "imagen": "Muelle.jpg",
+        "nombre": "Imagen 9",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 9"
+    },
+    {
+        "id": 10,
+        "imagen": "torrePisa.jpg",
+        "nombre": "Imagen 10",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 10"
+    },
+    {
+        "id": 11,
+        "imagen": "India.jpg",
+        "nombre": "Imagen 11",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 11"
+    },
+    {
+        "id": 12,
+        "imagen": "London.jpg",
+        "nombre": "Imagen 12",
+        "place": "algun lugar",
+        "country": "Lugar",
+        "dato": "Dato 12"
+    }
+]
+
+module.exports = cities;
